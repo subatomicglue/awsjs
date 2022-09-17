a1119dfa3ffc3b6c696aed638e7c0e0c200316c4
km <kmeinert@aviahealthinnovation.com>
2022-09-17 11:10:29 -0500

    added usage

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A library useful for deploying web assets to S3
 
+## Features
+
 AWS s3 bucket uploader with:
 - maximum upload performance
   - files upload in parallel
@@ -16,4 +18,40 @@ Methods exposed:
 - listObjects
 - deleteObjects
 - clearBucket
+
+## Usage
+Example `upload.js` script, used to deploy a web app to s3
+```
+const aws = require('./aws')
+
+// look in ~/.aws/credentials for which profile to use
+var credentials = new aws.AWS.SharedIniFileCredentials({profile: 'myprofile'});
+aws.AWS.config.credentials = credentials;
+
+let s3 = aws.createS3(
+  "mys3bucketname",
+  "us-west-2",
+);
+
+(async () => {
+  let subdir = ""; // dirname, no trailing slash. "" for root dir
+
+  // clear the entire bucket of any object prefixed with ${subdir}
+  // note: if we ever share bucket with other uploader scripts, we'll add white/blacklist items so we dont blow away resources from those other scripts
+  await s3.clearBucket( { whitelist: [`^${subdir}`], blacklist: [] })
+
+  // upload the entire local filesystem "dist/" directory (e.g. some webpack bundle) to s3://mys3bucketname/${subdir}
+  await s3.uploadDir( "dist/", `${subdir}${subdir != "" ? '/' : ''}`, {
+    cache: true,
+    batch_size: 8,
+    whitelist: [],
+    blacklist: [],
+    nocache_patterns: ['index.html$', 'index.html.gz$']
+  });
+
+  // upload the [src] buffer to 2 s3 objects:  'foldername' and 'foldername/index.html', without caching
+  let src = "some data that I put here"
+  await www.uploadFileData( src, "text/html", [`foldername`, `foldername/index.html`], { cache: false } );
+})();
+```
 
--- aws.js
+++ aws.js
@@ -16,7 +16,7 @@ let nondestructive = false;
 // s3.deleteObjects( params ) : Promise
 // s3.clearBucket( options = { whitelist: [], blacklist: [] } ) : Promise
 //
-// Note: the white/blacklisting is a pretty convenient way to upload/delete things in a targeted way
+// Note: the white/blacklisting is a pretty convenient way to upload/delete huge batches of things in a targeted way
 //
 function S3Proto( _s3, _bucketname ) {
   let s3 = _s3;
@@ -242,16 +242,37 @@ function S3Proto( _s3, _bucketname ) {
 
 // this module exports a single object "s3" which has methods on it like uploadDirs uploadFile clearBucket, etc...
 //
-// let aws = require( "./aws-utils" )
-// let s3 = aws.s3(
-//   bucketname,
+// const aws = require('./aws')
+//
+// // look in ~/.aws/credentials for which profile to use
+// var credentials = new aws.AWS.SharedIniFileCredentials({profile: 'myprofile'});
+// aws.AWS.config.credentials = credentials;
+//
+// let s3 = aws.createS3(
+//   "mys3bucketname",
 //   "us-west-2",
-//   "AKIAACCESSKEYFROMAWS",
-//   "jasdklfjasdklfjasSecretKeyFromAWSsadjfklasdjkfa",
 // );
 //
-// // upload src buffer to 2 s3 objects:  foldername and foldername/index.html, without caching
-// await www.uploadFileData( src, "text/html", [`foldername`, `foldername/index.html`], { cache: false } );
+// (async () => {
+//   let subdir = ""; // dirname, no trailing slash. "" for root dir
+//
+//   // clear the entire bucket of any object prefixed with ${subdir}
+//   // note: if we ever share bucket with other uploader scripts, we'll add white/blacklist items so we dont blow away resources from those other scripts
+//   await s3.clearBucket( { whitelist: [`^${subdir}`], blacklist: [] })
+//
+//   // upload the entire local filesystem "dist/" directory (e.g. some webpack bundle) to s3://mys3bucketname/${subdir}
+//   await s3.uploadDir( "dist/", `${subdir}${subdir != "" ? '/' : ''}`, {
+//     cache: true,
+//     batch_size: 8,
+//     whitelist: [],
+//     blacklist: [],
+//     nocache_patterns: ['index.html$', 'index.html.gz$']
+//   });
+//
+//   // upload the [src] buffer to 2 s3 objects:  'foldername' and 'foldername/index.html', without caching
+//   let src = "some data that I put here"
+//   await www.uploadFileData( src, "text/html", [`foldername`, `foldername/index.html`], { cache: false } );
+// })();
 module.exports.createS3 = (
     bucketname,
     region = "us-west-2",
